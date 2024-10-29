232eb8c921f668f136ca6550027d8f50881439a0
Walkeron-ua <177946022+Walkeron-ua@users.noreply.github.com>
2024-10-29 16:16:48 +0200

    Add three button & darkening effect
    -Three buttons: reset, and tho of them which alow u choose black or
     random color.
    -Add darkening effect where each interaction darkness the square by 10%
    -Started from 10% opacity.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container");
 const gridSizeBtn = document.querySelector("#gridSizeBtn");
+const colorBlackBtn = document.querySelector("#colorBlackBtn");
+const colorRandomBtn = document.querySelector("#colorRandomBtn");
 let gridSize = 16;
 
 gridSizeBtn.addEventListener("click", () => {
@@ -12,16 +14,32 @@ gridSizeBtn.addEventListener("click", () => {
 });
 
 function createSquares(gridSize) {
+    let colored = true;
     container.innerHTML = '';
     let amountSquares = gridSize * gridSize
+    colorBlackBtn.addEventListener("click", () => colored = false)
+    colorRandomBtn.addEventListener("click", () => colored = true)
     for (i = 0; i < amountSquares; i++) { 
         const newSquare = document.createElement("div");
         newSquare.style.width = `${960 / gridSize}px`
         newSquare.style.height = `${960 / gridSize}px`
-        newSquare.addEventListener("mouseenter", () => randomColor(newSquare));
+        newSquare.style.opacity = '0';
+        newSquare.addEventListener("mouseenter", () => {
+            if (colored) {
+                randomColor(newSquare);
+            }else {
+                newSquare.style.backgroundColor = 'black';
+            }
+            let opacity = Number(newSquare.style.opacity);
+            if (opacity < 1) {
+                newSquare.style.opacity = `${opacity+0.1}`;
+            }
+        });
         container.appendChild(newSquare);
     }
 }
+
+
 
 function randomNumber() {
     return Math.floor(Math.random() * 255) + 1;
@@ -29,5 +47,8 @@ function randomNumber() {
 function randomColor(newSquare) { 
     return newSquare.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
 }
+
+//Reset button
+document.querySelector("#resetGrid").addEventListener("click", () => createSquares(gridSize))
 
 createSquares(gridSize);
