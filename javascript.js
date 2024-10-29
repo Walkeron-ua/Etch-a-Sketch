0dc3949b5ad1442e7a303c75186b63a722f20e5a
Walkeron-ua <177946022+Walkeron-ua@users.noreply.github.com>
2024-10-29 13:53:08 +0200

    Added a button to allow users to set the grid size
    -Implemented an event listener on the button that prompts the user
     for a number between 1 and 100.
    -When a valid number is entered, it calls the createSquares() function
     to generate a new grid based on the user-defined size.
    -Wrapped the loop that creates squares in the createSquares() function
     for better organization.
    -Made the squares change size automatically based on the chosen grid
     size.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,26 @@
 const container = document.querySelector(".container");
-for (i = 0; i < 256; i++) { 
-    const newSquare = document.createElement("div");
-    newSquare.addEventListener("mousemove", () => newSquare.style.backgroundColor = "black")
-    container.appendChild(newSquare);
+const gridSizeBtn = document.querySelector("#gridSizeBtn");
+let gridSize = 16;
+
+gridSizeBtn.addEventListener("click", () => {
+    let input;
+    do {
+        input = Number(prompt("Write a number from 1 to 100 to set up size of grid."));
+    } while (isNaN(input) || input < 1 || input > 100);
+    gridSize = input;
+    createSquares(gridSize)
+});
+
+function createSquares(gridSize) {
+    container.innerHTML = '';
+    let amountSquares = gridSize * gridSize
+    for (i = 0; i < amountSquares; i++) { 
+        const newSquare = document.createElement("div");
+        newSquare.style.width = `${960 / gridSize}px`
+        newSquare.style.height = `${960 / gridSize}px`
+        newSquare.addEventListener("mouseenter", () => newSquare.style.backgroundColor = "black");
+        container.appendChild(newSquare);
+    }
 }
+
+createSquares(gridSize);
